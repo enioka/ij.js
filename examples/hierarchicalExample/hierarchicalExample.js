e5d202baa606c701eb4c35695b4c473d5b259213
Augustin Casse <augustincasse@orange.fr>
2016-01-19 14:55:09 +0100

    all the examples function with the Default_HTML_Renderer

--- examples/hierarchicalExample/hierarchicalExample.js
+++ examples/hierarchicalExample/hierarchicalExample.js
@@ -92,42 +92,6 @@ var Renderer = {
         this.template = new enioka.ij.HTMLTemplate();
     },
 
-    /**
-     * @function
-     * @description
-     * @param rendering
-     * @returns {*}
-     */
-    getRenderingId : function(rendering){
-        return rendering.id;
-    },
-
-    /**
-     * @function
-     * @description
-     * @param rendering
-     * @returns {DOMTokenList}
-     */
-    getRenderingClasses : function(rendering){
-        return rendering.classList;
-    },
-
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderRowsContainer : function(){
-        return this.renderer.createElement("tbody");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param rowObject
-     * @param rowNumber
-     * @returns {Array}
-     */
     renderRow : function(rowObject, rowNumber){
         var rowArray = new Array(),
             order,
@@ -168,61 +132,6 @@ var Renderer = {
         return rowArray;
     },
 
-    /**
-     * @function
-     * @description
-     * @param renderedObject
-     * @param property
-     * @returns {*}
-     */
-    applyRowSpan : function(renderedObject, property){
-        this.renderer.addAttribute(renderedObject,
-            "rowspan",
-            property);
-        return renderedObject;
-    },
-
-    /**
-     * @function
-     * @descrpition
-     * @param renderedObject
-     * @param property
-     * @returns {*}
-     */
-    applyColSpan : function(renderedObject, property){
-        this.renderer.addAttribute(renderedObject,
-            "colspan",
-            property);
-        return renderedObject;
-    },
-
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderRowContainer : function(){
-        var row = this.renderer.createElement("tr");
-        return row;
-    },
-
-    /**
-     * @function
-     * @descrption
-     * @param label
-     * @returns {*}
-     */
-    renderSubTotalHeader : function(label){
-        return this.renderer.createElementWithText("th", label + " summary");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param columnObject
-     * @param columnNumber
-     * @returns {Array}
-     */
     renderColumn : function(columnObject, columnNumber) {
         var columnArray = new Array(),
             order,
@@ -246,52 +155,6 @@ var Renderer = {
         return columnArray;
     },
 
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderLeftUpperCorner : function(){
-        return this.renderer.createElement("th");
-    },
-
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderColumnsLevelContainer : function(){
-        return this.renderer.createElement("tr");
-    },
-
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderColumnsContainer : function(){
-        return this.renderer.createElement("thead");
-    },
-
-    /**
-     * @function
-     * @descrpition
-     * @param renderedObject
-     * @returns {*|{id, object, label, order, rendering, open, visible}}
-     */
-    renderSummary : function(renderedObject){
-        return this._createRenderedJSON();
-    },
-
-    /**
-     * @function
-     * @description
-     * @param rowsNumbers
-     * @param columnsNumbers
-     * @param cellData
-     * @param eventsCallBacks
-     * @returns {*}
-     */
     renderCell : function(rowsNumbers, columnsNumbers, cellData, eventsCallBacks){
         if (cellData && cellData.length > 0) {
             var cell =  this.renderer.createElementWithText("td", cellData[0]);
@@ -318,182 +181,24 @@ var Renderer = {
                 [this.template.getAttribute("columnHeader", "classPrefix") +
                 columnsNumbers[column]]);
         }
-    },
-
-    /**
-     * @function
-     * @description
-     * @param element
-     * @param child
-     * @returns {*|Node|XML}
-     */
-    appendChild : function(element, child) {
-        return this.renderer.appendChild(element, child);
-    },
-
-    /**
-     * @function
-     * @descrption
-     * @param id
-     * @param object
-     * @param label
-     * @param order
-     * @param rendering
-     * @param open
-     * @param hidden
-     * @returns {{id: *, object: *, label: *, order: *, rendering: *, open: *, hidden: *}}
-     * @private
-     */
-    _createRenderedJSON : function(id, object, label, order, rendering, open, hidden){
-        return {
-            "id" : id,
-            "object" : object,
-            "label" : label,
-            "order" : order,
-            "rendering" : rendering,
-            "open" : open,
-            "hidden" : hidden
-        };
-    },
-
-    /**
-     * @function
-     * @description
-     * @returns {Element|*}
-     */
-    renderContainer : function(){
-        return this.renderer.createElement("table",["table","table-bordered"]);
-    },
-
-    /**
-     * @function
-     * @description
-     * @param property
-     * @param element
-     * @param propertyValue
-     * @returns {*|void}
-     */
-    setCSSProperty : function(property, element, propertyValue){
-        return element.style.setProperty(property, propertyValue);
-    },
-
-    /**
-     * @function
-     * @description
-     * @param property
-     * @param element
-     * @returns {*|void}
-     */
-    emptyCSSProperty : function(property, element){
-        return element.style.setProperty(property, "");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param element
-     * @param elementType
-     * @param classes
-     * @returns {*}
-     */
-    addClasses : function(element, elementType, classes){
-        var classPrefix = this.template.getAttribute(elementType, "classPrefix");
-        for (var i = 0; i < classes.length; i++){
-            classes[i] = classPrefix + classes[i];
-        }
-        return this.renderer.addClasses(element, classes);
-    },
-
-    /**
-     * @function
-     * @description
-     * @param element
-     * @param id
-     * @returns {*}
-     */
-    setId : function(element, id){
-        element.id = id;
-        return element;
-    },
-
-    /**
-     * @function
-     * @description
-     * @param type
-     * @returns {*|string}
-     */
-    getClassPrefix : function(type){
-        return this.template.getAttribute(type, "classPrefix");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param type
-     * @returns {*|string}
-     */
-    getIdPrefix : function(type){
-        return this.template.getAttribute(type, "idPrefix");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param rendering
-     * @param events
-     * @returns {Node|*}
-     */
-    addEventsToRendering : function(rendering, events){
-        /// have to clone the header to clean up from previous events if needed
-        var newHeader = rendering.cloneNode(true);
-        for (var event in events){
-            newHeader.addEventListener(event, events[event]);
-        }
-        return newHeader;
-    },
-
-    hideRendering : function(rendering) {
-        return this.renderer.addCSS(rendering, "display", "none");
-    },
-
-    showRendering : function(rendering) {
-        return this.renderer.deleteCSSProperty(rendering, "display");
-    },
-
-    /**
-     * @function
-     * @description
-     * @param output
-     */
-    clearOutput : function(output) {
-        while (output.firstChild) {
-            output.removeChild(output.firstChild);
-        }
-    },
-
-    _getSummaryHoverCell : function(cellData) {
-        return this.renderer.createElementWithText("p","There is " + cellData + " characters shared").outerHTML;
     }
 };
 
-Renderer = Class.extend(enioka.ij.IIJRenderer, Renderer);
+Renderer = Class.extend(DefaultHTMLRenderer, Renderer);
+
+
+//
+//Controller
+//
+
 
 var Controller = {
-    /**
-     * @function
-     * @description
-     * @param modelet
-     */
+    
     initialize : function(parent, core){
         this.component = parent;
         this.core = core;
     },
 
-    /**
-     * @function
-     * @description
-     * @param event
-     */
     onCellHover : function(event){
         info_debug("onCellHover");
         var classes = event.target.className.split(" ");
@@ -523,11 +228,6 @@ var Controller = {
         alert("there is " + cellData[0] + " characters shared");
     },
 
-    /**
-     * @function
-     * @description
-     * @param event
-     */
     onCellOut : function(event){
         info_debug("onCellHover");
         var classes = event.target.className.split(" ");
@@ -550,32 +250,16 @@ var Controller = {
             event.target);
     },
 
-    /**
-     * @function
-     * @description
-     * @param event
-     * @param headerNode
-     */
     onHeaderClick : function(event, headerNode){
         console.log("headerClick", headerNode);
         this.core.toggleHeader(headerNode);
         this.core.refresh(this.core.getHeaderType(headerNode))
     },
 
-    /**
-     * @function
-     * @description
-     * @param event
-     */
     onHeaderHover : function(event){
         info_debug("onHeaderHover", event);
     },
 
-    /**
-     * @function
-     * @description
-     * @param event
-     */
     onHeaderOut : function(event){
         info_debug("onHeaderOut", event);
     }
@@ -583,24 +267,18 @@ var Controller = {
 
 Controller = Class.extend(enioka.ij.IIJController, Controller);
 
+
+//
+//Aggregator
+//
+
+
 var Aggregator = {
-    /**
-     * @function
-     * @description
-     * @param modelet
-     */
+
     initialize : function(parent){
         this.component = parent;
     },
 
-    /**
-     * @function
-     * @description
-     * @param rowsObjects
-     * @param columnsObjects
-     * @param filter
-     * @returns {Array}
-     */
     aggregateData : function(rowsObjects, columnsObjects, filter) {
         var result = new Array();
         for (var i = 0; i < rowsObjects.length; i++){
